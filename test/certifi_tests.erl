c8e2763c5fcedfef797edab2f68ad7cc446232a0
Benoit Chesneau <bchesneau@gmail.com>
2018-04-02 15:34:13 +0200

    use the cacert file from curl

--- test/certifi_tests.erl
+++ test/certifi_tests.erl
@@ -10,7 +10,7 @@ reproducible_module_test() ->
 
 cacerts_test_() ->
     Certs = [Cert1, Cert2, Cert3 | _] = certifi:cacerts(),
-    [?_assertEqual(132, length(Certs))
+    [?_assertEqual(133, length(Certs))
     ,?_assertMatch(<<48,130,2,148,48,130,2,26,160,3,2,1,2,2,8,44,41,156,91,22,237,5,_/binary>>, Cert1)
     ,?_assertMatch(<<48,130,5,235,48,130,3,211,160,3,2,1,2,2,8,86,182,41,205,52,188,_/binary>>, Cert2)
     ,?_assertMatch(<<48,130,2,141,48,130,2,20,160,3,2,1,2,2,8,117,230,223,203,193,_/binary>>, Cert3)
