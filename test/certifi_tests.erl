88684adafa3c97a64ab10953b21ecb7a66e034c3
Garth Kidd <garth@garthk.com>
2020-06-02 17:09:26 +1000

    Adjust certificate count in test.

--- test/certifi_tests.erl
+++ test/certifi_tests.erl
@@ -10,7 +10,7 @@ reproducible_module_test() ->
 
 cacerts_test_() ->
     Certs = [Cert1, Cert2, Cert3 | _] = certifi:cacerts(),
-    [?_assertEqual(138, length(Certs))
+    [?_assertEqual(137, length(Certs))
     ,?_assertMatch(<<48,130,6,75,48,130,4,51,160,3,2,1,2,2,17,0,217,181,67,127,_/binary>>, Cert1)
     ,?_assertMatch(<<48,130,5,207,48,130,3,183,160,3,2,1,2,2,20,8,22,95,138,76,_/binary>>, Cert2)
     ,?_assertMatch(<<48,130,2,43,48,130,1,177,160,3,2,1,2,2,10,123,113,182,130, _/binary>>, Cert3)
