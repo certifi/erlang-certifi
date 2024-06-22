5dde0093ca59e1b4b21040b1dd711e21be295c57
Kian-Meng Ang <kianmeng@cpan.org>
2024-06-23 00:49:46 +0800

    CI housekeeping
    This PR ensures all CI jobs pass without errors.
    
    List of changes:
    - add support for OTP26/27 in CI
    - bump actions/checkout
    - drop support for OTP19 (not supported in official Docker image) from CI
    - drop support for OTP20 (not supported by rebar3_hex) from CI
    - fix test errors due to updated certs
    - remove Travis CI config
    - resolve dialyzer error of unknown function public_key:pem_decode/1 in
      OTP26/27

--- test/certifi_tests.erl
+++ test/certifi_tests.erl
@@ -10,9 +10,9 @@ reproducible_module_test() ->
 
 cacerts_test_() ->
     Certs = [Cert1, Cert2, Cert3 | _] = certifi:cacerts(),
-    [?_assertEqual(128, length(Certs))
-    ,?_assertMatch(<<48,130,5,192,48,130,3,168,160,3,2,1,2,2,16,30,191,89,80,184,_/binary>>, Cert1)
-    ,?_assertMatch(<<48,130,2,101,48,130,1,235,160,3,2,1,2,2,16,120,143,39,92,_/binary>>, Cert2)
-    ,?_assertMatch(<<48,130,5,239,48,130,3,215,160,3,2,1,2,2,8,13,211,227,188,_/binary>>, Cert3)
+    [?_assertEqual(147, length(Certs))
+    ,?_assertMatch(<<48,130,5,179,48,130,3,155,160,3,2,1,2,2,16,33,156,84,45,232,_/binary>>, Cert1)
+    ,?_assertMatch(<<48,130,2,66,48,130,1,201,160,3,2,1,2,2,16,54,58,150,140,_/binary>>, Cert2)
+    ,?_assertMatch(<<48,130,5,108,48,130,3,84,160,3,2,1,2,2,20,84,22,191,59,_/binary>>, Cert3)
     ,?_assertMatch(<<48,130,3,117,48,130,2,93,160,3,2,1,2,2,11,4,0,0,0,0,1,21,75,90,195,148,48,13,6,_/binary>>, lists:last(Certs))
     ].
