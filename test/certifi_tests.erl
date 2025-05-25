76cb38d07780131c04e9b2e2ba82b5be4232cc6e
benoitc <bchesneau@gmail.com>
2025-05-25 22:52:12 +0200

    bump certificates

--- test/certifi_tests.erl
+++ test/certifi_tests.erl
@@ -10,9 +10,9 @@ reproducible_module_test() ->
 
 cacerts_test_() ->
     Certs = [Cert1, Cert2, Cert3 | _] = certifi:cacerts(),
-    [?_assertEqual(150, length(Certs))
+    [?_assertEqual(143, length(Certs))
     ,?_assertMatch(<<48,130,5,169,48,130,3,145,160,3,2,1,2,2,16,105,38,9,126,128,_/binary>>, Cert1)
     ,?_assertMatch(<<48,130,5,169,48,130,3,145,160,3,2,1,2,2,16,115,59,48,4,72,_/binary>>, Cert2)
     ,?_assertMatch(<<48,130,2,35,48,130,1,169,160,3,2,1,2,2,20,22,21,199,195,216,_/binary>>, Cert3)
-    ,?_assertMatch(<<48,130,3,117,48,130,2,93,160,3,2,1,2,2,11,4,0,0,0,0,1,21,75,90,195,148,48,13,6,_/binary>>, lists:last(Certs))
+    ,?_assertMatch(<<48,130,4,145,48,130,3,121,160,3,2,1,2,2,4,69,107,80,84,48,_/binary>>, lists:last(Certs))
     ].
